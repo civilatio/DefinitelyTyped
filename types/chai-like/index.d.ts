2b5a9f5e187f8e215eff3f364d9f69aa1d7ea265
Tobias Stangl <tobias.stangl@msg.group>
2019-03-21 07:58:46 +0100

    Introducing types for chai like

---
+++ types/chai-like/index.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for chai-things
+// Project: https://github.com/zation/chai-like
+// Definitions by: David Broder-Rodgers <https://github.com/civilatio>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference types="chai" />
+
+declare namespace Chai {
+        interface Something extends Assertion {
+        (): any;
+        that: Assertion;
+        with: Assertion;
+        like: Like
+    }
+
+    interface Like {
+        (value: any): Assertion;
+    }
+}
+
+interface Array<T> {
+    should: Chai.ArrayAssertion;
+}
+
+declare module "chai-like" {
+    function chaiLike(chai: any, utils: any): void;
+    namespace chaiLike { }
+    export = chaiLike;
+}
